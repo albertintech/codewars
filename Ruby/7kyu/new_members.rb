ef040443714c9eadb61891d089a834a9236129d3
Albert Ramos <albertramos.dev@gmail.com>
2021-11-12 13:40:28 -0700

    Add solution

---
+++ Ruby/7kyu/new_members.rb
@@ -0,0 +1,50 @@
+=begin
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values stating whether the respective member is to be placed in the senior or open category.
+
+Example:
+input =  [(18, 20), (45, 2), (61, 12), (37, 6), (21, 21), (78, 9)]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+-Given a nested array where each element represents a member as a pair of numbers:
+  -First number is person's age
+  -Second number is a person's handicap (from 2 to +26)
+-Every member is given the base designation "Open" unless:
+  -Member is at least 55 years old with a handicap greater than 7
+  -If above is true, member is designated a "Senior"
+-Program should designate each member as "Open" or "Senior"
+
+Input: Nested array
+Output: Array of strings
+
+-Scan each array element, => each sub-array
+-Scan each sub-array
+-For each sub-array, return string "Open" unless:
+  -First number is >= 55 AND second number > 7
+  -Return string "Senior"
+
+=end
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+
+def open_or_senior(data)
+  membership = []
+  data.map do |i|
+    if (i[0] >= 55 && i[1] > 7)
+      membership << "Senior"
+    else
+      membership << "Open"
+    end
+  end
+  membership
+end
+
+p open_or_senior(input)
+
+# Had to look at a solution. Was close. Mistakenly thought I had to use another iterating method to access nested array so was working one layer too deep. Logic was sound, implementation flawed.
