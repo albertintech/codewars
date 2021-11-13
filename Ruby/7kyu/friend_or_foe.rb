4b3a52abcec183ac5eb142fea72f0cf59e9aed52
Albert Ramos <albertramos.dev@gmail.com>
2021-11-13 09:14:50 -0700

    Update file

--- Ruby/7kyu/friend_or_foe.rb
+++ Ruby/7kyu/friend_or_foe.rb
@@ -9,4 +9,39 @@ Example:
 people: ["Ryan", "Kieran", "Mark"] => friends: ["Ryan", "Mark"]
 
 Note: keep the original order of the names in the output.
+
+Input: Array of strings
+Output: Sub array of strings
+
+-Iterate over array
+-Find length of each string
+ -Keep strings <= 4 characters long
+ -Discard other strings
+-Return sub array
+
 =end
+
+names = ["Ryan", "Jimmy", "123", "4", "Cool Man"]
+
+# def friend(friends)
+#   friends.select { |name| name.size == 4}
+# end
+
+# def friend(friends)
+#   friends.select do |name|
+#     name.size == 4
+#   end
+# end
+
+def friend(friends)
+  arr = []
+  friends.each do |name|
+    if name.size == 4
+      arr << name
+    end
+  end
+  arr
+end
+
+
+p friend(names)
